3d6a0f6149205fe7ff1c07b77d2e40a9616230f2
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-02-18 19:21:47 +0600

    adding views and route them on web.php

--- blog/resources/views/AboutPage.blade.php
+++ blog/resources/views/AboutPage.blade.php
@@ -1,1 +1,1 @@
-<h1>This is About page</h1>
+<h1>This is about page</h1>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('HomePage');
 });
+
+Route::get('/about', function () {
+    return view('AboutPage');
+});
+
+Route::get('/contact', function () {
+    return view('ContactPage');
+});
+
