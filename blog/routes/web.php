e014145ad94669685200f41303daefc0a570400d
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-02-18 19:15:57 +0600

    blade features

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -3,18 +3,3 @@
 use Illuminate\Support\Facades\Route;
 
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/hello',function(){
-    return "Hello World";
-
-});
-
-Route::get('/name',function(){
-    return "My name is Biki";
-
-});
