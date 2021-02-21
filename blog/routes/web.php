5fd8317a1fa30fbd958d17faf92042c8c68e2252
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-02-21 19:55:14 +0600

    errors solved

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/Name/{firstName}/{MiddleName}/{lastName}', 'DemoController@MyName');
+Route::get('/Name/{firstName}/{MiddleName}/{lastName}', 'App\Http\Controllers\DemoController@MyName');
