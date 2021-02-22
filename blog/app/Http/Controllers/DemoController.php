41dc425c69f66eba6af5c3f0d560514db34823d3
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-02-22 20:21:07 +0600

    demoview

--- blog/app/Http/Controllers/DemoController.php
+++ blog/app/Http/Controllers/DemoController.php
@@ -8,6 +8,6 @@ class DemoController extends Controller
 {
     function MyName($firstName, $MiddleName, $lastName){
 
-        return " First Name: " . $firstName . " Middle Name: ". $MiddleName. " Last Name: ". $lastName;
+       return view('DemoView');
     }
 }
