09568d316a8136fd3e3fc24398abc32de3a301f3
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-02-23 19:48:01 +0600

    done!

--- blog/resources/views/DemoView.blade.php
+++ blog/resources/views/DemoView.blade.php
@@ -1,4 +1,4 @@
-<h1>First Name: {{firstKey}}</h1>
-<h1>Middle Name:{{middleKey}}</h1>
-<h1>Last Name:{{lastKey}}</h1>
+<h1>First Name: {{$firstKey}}</h1>
+<h1>Middle Name:{{$middleKey}}</h1>
+<h1>Last Name:{{$lastKey}}</h1>
 
