44c6e055d29999dbb1649fe7a361fd80ec21f0a4
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-02-26 19:22:17 +0600

    single action controller

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,29 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix'=>'account'],function(){
-
-    Route::get('/profile', function(){
-        return "Profile";
-    });
-
-
-    Route::get('/login', function(){
-        return "login";
-    });
-
-
-    Route::get('/logout', function(){
-        return "logout";
-    });
-
-
-    Route::get('/signup', function(){
-        return "signup";
-    });
-
-    Route::get('/updateprofile', function(){
-        return "updateprofile";
-    });
-
-});
+Route::get('/','App\Http\Controllers\MyController');
