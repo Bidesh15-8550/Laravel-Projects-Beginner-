a67c5fcb630825aa4f070d1262b5dfcfd5aee7de
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-02-26 18:35:51 +0600

    routegroup view

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,4 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::group(['prefix'=>'account'],function(){
 
+    Route::get('/profile', function(){
+        return "Profile";
+    });
+
+
+    Route::get('/login', function(){
+        return "login";
+    });
+
+
+    Route::get('/logout', function(){
+        return "logout";
+    });
+
+
+    Route::get('/signup', function(){
+        return "signup";
+    });
+
+    Route::get('/updateprofile', function(){
+        return "updateprofile";
+    });
+
+});
