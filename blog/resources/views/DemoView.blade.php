3d23a57dbbf0803b65a4e9a972c5c54a4d59fd5e
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-02-26 18:26:39 +0600

    Route group to view

--- blog/resources/views/DemoView.blade.php
+++
@@ -1,4 +0,0 @@
-<h1>First Name: {{$firstKey}}</h1>
-<h1>Middle Name:{{$middleKey}}</h1>
-<h1>Last Name:{{$lastKey}}</h1>
-
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,6 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/Name/{firstName}/{MiddleName}/{lastName}', 'App\Http\Controllers\DemoController@MyName');
 
-//route location
