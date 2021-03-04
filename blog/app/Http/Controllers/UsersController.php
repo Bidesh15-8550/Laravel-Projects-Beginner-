6d818034b9a9f192ab0ceb193abd1ffbe1417128
Bidesh15-8550 <bideshbiswas@ymail.com>
2021-03-04 17:52:23 +0600

    form validation

--- blog/app/Http/Controllers/UsersController.php
+++ blog/app/Http/Controllers/UsersController.php
@@ -10,8 +10,8 @@ class UsersController extends Controller
     function getData(Request $req)
     {
         $req ->validate([
-            'username' => 'required',
-            'userpassword' => 'required'
+            'username' => 'required | max:10',
+            'userpassword' => 'required | min:5'
         ]);
         return $req->input();
     }
